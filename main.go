f1bea31d2e79cf8a45bd6a20b6949ba79c4d8546
andev0x <anvndev@gmail.com>
2025-07-24 20:37:12 +0700

    fix(feat): telegram bot connection

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
 	"time"
 
@@ -9,30 +10,50 @@ import (
 	"github.com/andev0x/tele-news-bot/feed"
 	"github.com/andev0x/tele-news-bot/notify"
 	"github.com/andev0x/tele-news-bot/store"
+	tele "gopkg.in/telebot.v3"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 	sentItems := store.LoadSentItems()
 
-	for {
-		var wg sync.WaitGroup
-		for _, feedURL := range cfg.Feeds {
-			wg.Add(1)
-			go func(url string) {
-				defer wg.Done()
-				items := feed.FetchFeed(url)
-				for _, item := range items {
-					if !sentItems[item.Link] {
-						message := fmt.Sprintf("%s\n%s", item.Title, item.Link)
-						notify.SendMessage(cfg.BotToken, cfg.ChatID, message)
-						store.MarkSent(item.Link)
-						sentItems[item.Link] = true
-					}
-				}
-			}(feedURL)
-		}
-		wg.Wait()
-		time.Sleep(10 * time.Minute)
+	pref := tele.Settings{
+		Token:  cfg.BotToken,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
 	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Bot started! I will now check for news every 10 minutes.")
+	})
+
+	go func() {
+		for {
+			var wg sync.WaitGroup
+			for _, feedURL := range cfg.Feeds {
+				wg.Add(1)
+				go func(url string) {
+					defer wg.Done()
+					items := feed.FetchFeed(url)
+					for _, item := range items {
+						if !sentItems[item.Link] {
+							message := fmt.Sprintf("%s\n%s", item.Title, item.Link)
+							notify.SendMessage(b, cfg.ChatID, message)
+							store.MarkSent(item.Link)
+							sentItems[item.Link] = true
+						}
+					}
+				}(feedURL)
+			}
+			wg.Wait()
+			time.Sleep(10 * time.Minute)
+		}
+	}()
+
+	b.Start()
 }
--- notify/telegram.go
+++ notify/telegram.go
@@ -1,25 +1,22 @@
 package notify
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
+	"strconv"
+
+	tele "gopkg.in/telebot.v3"
 )
 
-func SendMessage(botToken, chatID, message string) {
-	endpoint := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
-	resp, err := http.PostForm(endpoint, url.Values{
-		"chat_id": {chatID},
-		"text":    {message},
-	})
+func SendMessage(b *tele.Bot, chatID, message string) {
+	i, err := strconv.ParseInt(chatID, 10, 64)
+	if err != nil {
+		log.Println("Error parsing chatID", err)
+	}
+	chat := &tele.Chat{ID: i}
+
+	_, err = b.Send(chat, message)
 	if err != nil {
 		log.Println("Telegram error:", err)
 		return
 	}
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			log.Println("error closing telegram response body:", err)
-		}
-	}()
 }
